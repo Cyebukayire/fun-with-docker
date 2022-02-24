d6a4955327072e9568d18e1c83775d587d2b3c0d
cyebukayire <peacecyebukayire@gmail.com>
2022-02-24 10:56:00 +0200

    chore: dockerised the app

--- Hello-Docker/app.js
+++ Hello-Docker/app.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express = require ('express')
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
