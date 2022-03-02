62953d26c97873d03469d1700996c68b3902a579
cyebukayire <peacecyebukayire@gmail.com>
2022-03-02 17:26:19 +0200

    chore: added docker compose commands

--- Hello-Docker/app.js
+++ Hello-Docker/app.js
@@ -9,3 +9,24 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
+
+app.get("/users", (req, res) => {
+    res.send({
+        id: 1,
+        name: "Peace"
+        // users: [
+        //     [
+        //         id= 1,
+        //         name= "Peace"
+        //     ],
+        //     [
+        //         id= 2,
+        //         name= "Musa"
+        //     ],
+        //     [
+        //         id= 3,
+        //         name= "Gedeon"
+        //     ],
+        // ]
+    })
+})
