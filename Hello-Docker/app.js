c0d1d6c4882bf76d8d178b71ceb41d941180a169
cyebukayire <peacecyebukayire@gmail.com>
2022-03-03 16:04:40 +0200

    Create REST APIs

--- Hello-Docker/app.js
+++ Hello-Docker/app.js
@@ -2,15 +2,15 @@ const express = require ('express')
 const mongoose = require("mongoose")
 const app = express()
 const PORT = 3000
+const routes = require('./routes')
 
 // Connect to MongoDB 
 mongoose
     .connect("mongodb://localhost:27017/learning", {useNewUrlParser: true})
     .then(() => {
         const app = express()
-        app.get('/', (req, res) => {
-            res.send("Hello, Customer!")
-        });
+        app.use(express.json())
+        app.use("/api", routes)
 
         app.listen(PORT, () => {
             console.log("Server started...")
